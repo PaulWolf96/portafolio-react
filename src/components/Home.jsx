08a6ab71a521195e26c452c1ef10a3dc979195ba
PaulWolf96 <pablolobo96@gmail.com>
2023-12-14 10:16:02 -0300

    styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,25 +15,21 @@ import '../styles/home.css';
 
 
 const Home = () => {
-
-
-  
-
   return (
     <main>
-      <section className="section-info">
-        <div className="img-perfil">
-          <img src={LogoPerfil} alt="foto perfil" />
-        </div>
-        <div className="descripcion-general">
-          <h1>Pablo  Leonardo  Lobo</h1>
-          <i className="subtitulo">Front End | React</i>
-          <p>"Hola, Bienvenido a mi Portafolio!<br />
-            Estoy buscando mi primer experiencia laboral como desarrollador web, tengo muchas ganas de entrar al mundo IT y
-            poder crecer en él ya que siempre me interesó la programación."
-          </p>
-          <img src={Globito} alt="" className="img-globito" width="600px"></img>
-        </div>
+      <section className="section-info animate__animated animate__backInDown">
+          <div className="img-perfil">
+            <img src={LogoPerfil} alt="foto perfil" />
+          </div>
+          <div className="descripcion-general">
+            <h1>Pablo  Leonardo  Lobo</h1>
+            <i className="subtitulo">Front End | React</i>
+            <p>"Hola, Bienvenido a mi Portafolio!<br />
+              Estoy buscando mi primer experiencia laboral como desarrollador web, tengo muchas ganas de entrar al mundo IT y
+              poder crecer en él ya que siempre me interesó la programación."
+            </p>
+            <img src={Globito} alt="" className="img-globito" width="600px"></img>
+          </div>
       </section>
 
       <article>
@@ -87,7 +83,10 @@ const Home = () => {
             </button>
           </div>
         </section>
+
         <section className="section-projects">Projects</section>
+
+        <section className="section-contact">Contacto</section>
       </article>
 
     </main>
