17c18df039383c34e734721d4b1b43bbbdd01f54
PaulWolf96 <pablolobo96@gmail.com>
2024-01-08 13:37:09 -0300

    responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header>
       <nav className="navbar fixed-top navbar-expand-md">
-        <div className="container-fluid">
+        <div className="container-fluid p-0">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
